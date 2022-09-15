3c896f5d3ca8af3f3baac03e6c244e0e850de1dd
SnowyyUwU <ulisesdraws@gmail.com>
2022-09-15 23:49:14 +0000

    updates :3

--- src/main/java/com/snowy/snowyminigames/command/ArenaCommand.java
+++ src/main/java/com/snowy/snowyminigames/command/ArenaCommand.java
@@ -8,7 +8,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 public class ArenaCommand implements CommandExecutor {
 
@@ -18,7 +17,7 @@ public class ArenaCommand implements CommandExecutor {
         this.minigame = minigame;
     }
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
