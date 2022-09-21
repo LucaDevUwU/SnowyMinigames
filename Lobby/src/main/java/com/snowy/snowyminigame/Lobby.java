01c27660110b61a6808c04b8f0a0a0f1312439d9
SnowyyUwU <ulisesdraws@gmail.com>
2022-09-21 00:46:42 +0000

    new Module

---
+++ Lobby/src/main/java/com/snowy/snowyminigame/Lobby.java
@@ -0,0 +1,12 @@
+package com.snowy.snowyminigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
